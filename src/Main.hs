7c59bdf17101ecbd5292766fb6098f2a9cda3703
Tomas Beranek <xberan46@stud.fit.vutbr.cz>
2022-02-23 00:31:56 +0100

    Add program argument parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,17 @@
-main = do
-  c <- getChar
-  putChar c
+import System.Environment
+import System.Exit
+
+main = getArgs >>= parseArgs >>= printNFSM . convertToNFSM . loadRLG
+
+loadRLG x = x
+convertToNFSM x = x
+printNFSM x = putStrLn x
+
+parseArgs ("-i":xs) = putStrLn "Loaded RLG" >> parseArgs xs >> exit
+parseArgs ("-1":xs) = putStrLn "Converted RLG to RRG" >> exit
+parseArgs ("-2":xs) = putStrLn "Converted RLG to NFSM" >> exit
+parseArgs (x:[]) = putStrLn "Reading from file" >> exit
+
+parseArgs [] = getContents
+
+exit = exitWith ExitSuccess
