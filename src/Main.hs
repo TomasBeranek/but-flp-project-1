969e34f91dd0c1b98555eb10fb8befa89acc99d5
TomasBeranek <xberan46@stud.fit.vutbr.cz>
2022-02-23 03:20:08 +0100

    Add implementation of RLG loading into RLG data type

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,62 @@
-import System.Environment
-import System.Exit
+import System.Environment (getArgs)
+import System.Exit (exitWith, ExitCode(ExitSuccess), ExitCode(ExitFailure))
+import System.IO (hPutStrLn, stderr)
 
-main = getArgs >>= parseArgs >>= printNFSM . convertToNFSM . loadRLG
+data RLG = RLG
+  { nonTerminals :: [Char]
+  , terminals :: [Char]
+  , startingSymbol  :: Char
+  , rules :: [(Char,[Char])]
+  }
 
-loadRLG x = x
+main = getArgs >>= parseArgs
+
+loadRLG str = createRLG (lines str)
+
+createRLG xs = RLG{ nonTerminals =  myUnique (removeChar ',' (xs !! 0)),
+                    terminals = myUnique (removeChar ',' (xs !! 1)),
+                    startingSymbol = (xs !! 2) !! 0,
+                    rules = myUnique (map parseRule (drop 3 xs))}
+
+removeChar c str = concat (mySplitOn ',' str)
+
+mySplitOn delim [] = []
+mySplitOn delim xs = [(takeWhile (/= delim) xs)] ++ (mySplitOn delim (safeTail (dropWhile (/= delim) xs)))
+
+myUnique [] = []
+myUnique (x:xs) =
+  if x `elem` xs
+    then myUnique xs
+    else [x] ++ myUnique xs
+
+safeTail [] = []
+safeTail xs = tail xs
+
+parseRule xs =  createRule (removeRightArrow (mySplitOn '-' xs))
+
+removeRightArrow (x:[]) = [safeTail x]
+removeRightArrow (x:xs) = [x] ++ removeRightArrow(xs)
+
+createRule xs = ((xs !! 0) !! 0, xs !! 1)
+
 convertToNFSM x = x
-printNFSM x = putStrLn x
+printNFSM x = putStrLn "TBD"
+convertToRRG x = x
+printRLG rlg = putStrLn ("nonTerminals: " ++ show (nonTerminals rlg)) >>
+               putStrLn ("terminals: " ++ show (terminals rlg)) >>
+               putStrLn ("startingSymbol: " ++ show (startingSymbol rlg)) >>
+               putStrLn ("rules: " ++ show (rules rlg))
 
-parseArgs ("-i":xs) = putStrLn "Loaded RLG" >> parseArgs xs >> exit
-parseArgs ("-1":xs) = putStrLn "Converted RLG to RRG" >> exit
-parseArgs ("-2":xs) = putStrLn "Converted RLG to NFSM" >> exit
-parseArgs (x:[]) = putStrLn "Reading from file" >> exit
+printRRG x = putStrLn "TBD"
 
-parseArgs [] = getContents
+parseArgs ("-i":xs) = hPutStrLn stderr "DEBUG: Print RLG" >> loadInput xs >>= printRLG . loadRLG >> exit
+parseArgs ("-1":xs) = hPutStrLn stderr "DEBUG: Print RRG" >> loadInput xs >>= printRRG . convertToRRG . loadRLG  >> exit
+parseArgs ("-2":xs) = hPutStrLn stderr "DEBUG: Print NFSM" >> loadInput xs >>= printNFSM . convertToNFSM . convertToRRG . loadRLG
+parseArgs (x:_) = hPutStrLn stderr ("ERROR: Unknown argument '" ++ x ++ "'.") >> exitError
+
+loadInput (x:[]) = hPutStrLn stderr ("DEBUG: Reading file: " ++ x) >> readFile x
+loadInput [] = hPutStrLn stderr "DEBUG: Reading stdin" >> getContents
+loadInput _ = hPutStrLn stderr "ERROR: Invalid number of files (max 1)." >> exitError
 
 exit = exitWith ExitSuccess
+exitError = exitWith (ExitFailure 1)
