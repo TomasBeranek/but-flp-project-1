cd05afad307c81ff6a88a67144c644780f4f2291
TomasBeranek <xberan46@stud.fit.vutbr.cz>
2022-02-26 16:34:32 +0100

    Fix running out of names for non-terminals
    Input non-terminals can be only Char, but newly created non-terminals (from rule splitting) can be named 'A1', 'B1', ...

--- src/Main.hs
+++ src/Main.hs
@@ -5,11 +5,15 @@ import Debug.Trace (trace)
 import Data.List (elemIndex, sort)
 import Data.Maybe (fromJust)
 
-data GrammarType = GrammarType {  nonTerminals :: [Char],
-                  terminals :: [Char],
-                  startingSymbol  :: Char,
-                  rules :: [(Char,[Char])] }
+data RLG = RLG {  nonTerminalsRLG :: [Char],
+                  terminalsRLG :: [Char],
+                  startingSymbolRLG  :: Char,
+                  rulesRLG :: [(Char,[Char])] }
 
+data RRG = RRG {  nonTerminalsRRG :: [[Char]], -- might contain new terminals named "A123", "B21", ...
+                  terminalsRRG :: [Char],
+                  startingSymbolRRG  :: Char,
+                  rulesRRG :: [([Char],[Char])] } -- might contain new terminals on both sides
 data NFSM = NFSM {  states :: [Int],
                     alphabet :: [Char],
                     transitions  :: [(Int,Char,Int)],
@@ -20,10 +24,10 @@ main = getArgs >>= parseArgs
 
 loadRLG str = createRLG (lines str)
 
-createRLG xs = GrammarType {  nonTerminals =  sort (myUnique (removeChar ',' (xs !! 0))),
-                              terminals = myUnique (removeChar ',' (xs !! 1)),
-                              startingSymbol = (xs !! 2) !! 0,
-                              rules = myUnique (map parseRule (drop 3 xs)) }
+createRLG xs = RLG {  nonTerminalsRLG =  sort (myUnique (removeChar ',' (xs !! 0))),
+                      terminalsRLG = myUnique (removeChar ',' (xs !! 1)),
+                      startingSymbolRLG = (xs !! 2) !! 0,
+                      rulesRLG = myUnique (map parseRule (drop 3 xs)) }
 
 removeChar c str = concat (mySplitOn c str)
 
@@ -46,15 +50,15 @@ removeRightArrow (x:xs) = [x] ++ removeRightArrow(xs)
 
 createRule xs = ((xs !! 0) !! 0, xs !! 1)
 
-convertToRRG rlg = GrammarType {  nonTerminals =  sort x,
-                                  terminals = sort (terminals rlg),
-                                  startingSymbol = startingSymbol rlg,
-                                  rules = y }
-                 where (x,y) = splitRLGRules (nonTerminals rlg) (rules rlg)
+convertToRRG rlg = RRG {  nonTerminalsRRG =  sort x,
+                          terminalsRRG = sort (terminalsRLG rlg),
+                          startingSymbolRRG = startingSymbolRLG rlg,
+                          rulesRRG = y }
+                 where (x,y) = splitRLGRules (nonTerminalsRLG rlg) (rulesRLG rlg)
 
-splitRLGRules nonTerminals [] = (nonTerminals, [])
+splitRLGRules nonTerminals [] = (map (:[]) nonTerminals, [])
 splitRLGRules nonTerminals (rule:rs) = if ruleIsRRG (snd rule)  --ruleIsRRG only needs the right side of the rule
-                                          then (restOfNonRerminals, [rule] ++ restOfRules)
+                                          then (restOfNonRerminals, ( [fst rule], snd rule ) : restOfRules) --convert left side of the rule to string
                                           else (allNonTerminals, restOfRules ++ newRules)
                                        where  (restOfNonRerminals, restOfRules) = splitRLGRules nonTerminals rs
                                               (allNonTerminals, newRules) = splitSingleRLGRule restOfNonRerminals rule
@@ -67,31 +71,32 @@ ruleIsRRG ruleRightSide = if  ruleRightSide == "#"
                             else False
 
 splitSingleRLGRule nonTerminals rule = if (last (snd rule)) `elem` ['A'..'Z']  --if the rule ends with nonTerminal
-                                          then splitSingleRLGNonTerminalRule nonTerminals (snd rule) (fst rule)
-                                          else splitSingleRLGTerminalRule nonTerminals (snd rule) (fst rule)
+    then splitSingleRLGNonTerminalRule nonTerminals (snd rule) [(fst rule)] --nonTerminals names need to be converted to strings
+    else splitSingleRLGTerminalRule nonTerminals (snd rule) [(fst rule)] --nonTerminals names need to be converted to strings
 
 splitSingleRLGNonTerminalRule nonTerminals ruleRightSide lastNT = if length ruleRightSide == 2
     then (nonTerminals, [(lastNT, ruleRightSide)])
-    else (allNonTerminals, [(lastNT, (head ruleRightSide):[freeNonTerminal])] ++ restOfRules)
+    else (allNonTerminals, [(lastNT, (head ruleRightSide):freeNonTerminal)] ++ restOfRules)
   where (allNonTerminals, restOfRules) = splitSingleRLGNonTerminalRule (freeNonTerminal:nonTerminals) (tail ruleRightSide) freeNonTerminal
-        freeNonTerminal = getFreeNonTerminal nonTerminals ['A'..'Z']
+        freeNonTerminal = getFreeNonTerminal nonTerminals getAllNonTerminalsNames
 
 splitSingleRLGTerminalRule nonTerminals ruleRightSide lastNT = if length ruleRightSide == 0
     then (nonTerminals, [(lastNT, "#")])
-    else (allNonTerminals, [(lastNT, (head ruleRightSide):[freeNonTerminal])] ++ restOfRules)
+    else (allNonTerminals, [(lastNT, (head ruleRightSide):freeNonTerminal)] ++ restOfRules)
   where (allNonTerminals, restOfRules) = splitSingleRLGTerminalRule (freeNonTerminal:nonTerminals) (tail ruleRightSide) freeNonTerminal
-        freeNonTerminal = getFreeNonTerminal nonTerminals ['A'..'Z']
+        freeNonTerminal = getFreeNonTerminal nonTerminals getAllNonTerminalsNames
 
-getFreeNonTerminal existingNT [] = '@' --hPutStrLn stderr "ERROR: Run out of possible non-terminals." >> exitError
+getAllNonTerminalsNames = [ [x] | x <- ['A'..'Z'] ] ++ [ [x] ++ (show y) | y <- [1..], x <- ['A'..'Z'] ]
+
 getFreeNonTerminal existingNT (possibleNT:nts) = if possibleNT `elem` existingNT
                                                   then getFreeNonTerminal existingNT nts
                                                   else possibleNT
 
-convertToNFSM rrg = NFSM {  states = renameNonTerminals (nonTerminals rrg) (nonTerminals rrg),
-                            alphabet = terminals rrg,
-                            transitions = createTransitions (nonTerminals rrg) (rules rrg),
-                            startState = fromJust $ elemIndex (startingSymbol rrg) (nonTerminals rrg),
-                            finalStates = createFinalStates (nonTerminals rrg) (rules rrg) }
+convertToNFSM rrg = NFSM {  states = renameNonTerminals (nonTerminalsRRG rrg) (nonTerminalsRRG rrg),
+                            alphabet = terminalsRRG rrg,
+                            transitions = createTransitions (nonTerminalsRRG rrg) (rulesRRG rrg),
+                            startState = fromJust $ elemIndex [startingSymbolRRG rrg] (nonTerminalsRRG rrg),
+                            finalStates = createFinalStates (nonTerminalsRRG rrg) (rulesRRG rrg) }
 
 renameNonTerminals _ [] = []
 renameNonTerminals allNonTerminals (nt:nts) = (fromJust $ elemIndex nt allNonTerminals):renameNonTerminals allNonTerminals nts
@@ -102,7 +107,7 @@ createTransitions allNonTerminals (r:rs) = if snd r == "#"
                                               else (startState, symbol, endState):(createTransitions allNonTerminals rs)
                                            where startState = fromJust $ elemIndex (fst r) allNonTerminals
                                                  symbol = head (snd r)
-                                                 endState = fromJust $ elemIndex (last (snd r)) allNonTerminals
+                                                 endState = fromJust $ elemIndex (tail (snd r)) allNonTerminals
 
 createFinalStates _ [] = []
 createFinalStates allNonTerminals (r:rs) = if snd r == "#"
@@ -115,14 +120,15 @@ printNFSM nfsm = putStrLn ("states: " ++ show (states nfsm)) >>
                  putStrLn ("startState: " ++ show (startState nfsm)) >>
                  putStrLn ("finalStates: " ++ show (finalStates nfsm))
 
-printGrammar rlg = putStrLn ("nonTerminals: " ++ show (nonTerminals rlg)) >>
-               putStrLn ("terminals: " ++ show (terminals rlg)) >>
-               putStrLn ("startingSymbol: " ++ show (startingSymbol rlg)) >>
-               putStrLn ("rules: " ++ show (rules rlg))
+printRLG rlg = putStrLn ("nonTerminals: " ++ show (nonTerminalsRLG rlg)) >>
+               putStrLn ("terminals: " ++ show (terminalsRLG rlg)) >>
+               putStrLn ("startingSymbol: " ++ show (startingSymbolRLG rlg)) >>
+               putStrLn ("rules: " ++ show (rulesRLG rlg))
 
-printRLG rlg = printGrammar rlg
-
-printRRG rrg = printGrammar rrg
+printRRG rrg = putStrLn ("nonTerminals: " ++ show (nonTerminalsRRG rrg)) >>
+               putStrLn ("terminals: " ++ show (terminalsRRG rrg)) >>
+               putStrLn ("startingSymbol: " ++ show (startingSymbolRRG rrg)) >>
+               putStrLn ("rules: " ++ show (rulesRRG rrg))
 
 parseArgs ("-i":xs) = hPutStrLn stderr "DEBUG: Print RLG" >> loadInput xs >>= printRLG . loadRLG >> exit
 parseArgs ("-1":xs) = hPutStrLn stderr "DEBUG: Print RRG" >> loadInput xs >>= printRRG . convertToRRG . loadRLG  >> exit
